287dd90231054349b0b686bba138153916e8fe5b
Davit <57663927+DavitV-yan@users.noreply.github.com>
2022-02-26 20:42:47 +0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -3,7 +3,7 @@ const {webkit, chromium, firefox} = require('playwright');
 (async () => {
   for (const browserType of [webkit, firefox, chromium]) {
     const browser = await browserType.launch({
-      headless: false,
+      headless: true,
     });
     const page = await browser.newPage();
     await page.goto('https://github.com/microsoft/playwright');
