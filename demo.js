0ee4fc51346891610f02f4d0580a177aa028eb54
Davit <57663927+DavitV-yan@users.noreply.github.com>
2022-02-26 20:23:18 +0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -2,7 +2,7 @@ const {webkit, chromium, firefox} = require("playwright");
 
 (async () => {
     for(const browserType of [webkit, chromium, firefox]){
-        const browser = await browserType.launch({headless: false});
+        const browser = await browserType.launch();
         const page = await browser.newPage();
         await page.goto("https://list.am");
         await page.screenshot({
