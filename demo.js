96adcfb55a12655eae8672f52859581fe2825b58
Davit <57663927+DavitV-yan@users.noreply.github.com>
2022-02-26 20:31:44 +0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,15 +1,16 @@
-const {webkit, chromium, firefox} = require("playwright");
+const {webkit, chromium, firefox} = require('playwright');
 
 (async () => {
-    for(const browserType of [webkit, chromium, firefox]){
-        const browser = await browserType.launch();
-        const page = await browser.newPage();
-        await page.goto("https://list.am");
-        await page.screenshot({
-            path: `screenshot-${browserType.name()}.png`,
-        });
-        await browser.close();
-        console.log(`success: `+ browserType.name());
-
-    }
+  for (const browserType of [webkit, firefox, chromium]) {
+    const browser = await browserType.launch({
+      headless: false,
+    });
+    const page = await browser.newPage();
+    await page.goto('https://github.com/microsoft/playwright');
+    await page.screenshot({
+      path: `screenshot-${browserType.name()}.png`,
+    });
+    await browser.close();
+    console.log('success: ' + browserType.name());
+  }
 })();
